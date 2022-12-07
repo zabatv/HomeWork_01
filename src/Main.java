813c079647c32e3a448beba41d24aef47d4b8264
home <germanpetrov08092009@gmail.com>
2022-12-07 17:32:54 +0300

    UNKNOWN

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763_789;
+        var box = 4;
+
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog += box;
+        cat += box;
+        paper += box;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
+    }
+
+
+
+
+
+}
